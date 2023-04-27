044f9062a1f6477bf1201fb7a0f255d9bb3561fc
dklee123 <hnna654@gmail.com>
2023-04-27 17:22:47 +0900

    9일차

---
+++ human/src/Day08/Ex06_MultipleInterface/SmartTelevisonEx.java
@@ -0,0 +1,29 @@
+package Day08.Ex06_MultipleInterface;
+
+public class SmartTelevisonEx {
+	
+	public static void main(String[] args) {
+		
+		SmartTelevision tv = new SmartTelevision();
+		
+		tv.turnOn();
+		tv.setVolume(20);
+		int channel = tv.channelSearch("SBS");
+		tv.setChannel(channel);
+		
+		String[] idolContents = tv.contentSearch("아이돌");
+		System.out.println("========== 검색된 컨텐츠 ==========");
+		
+		for (int i = 0; i < idolContents.length; i++) {
+			System.out.print(idolContents[i]);
+			if(i < idolContents.length-1)
+				System.out.print(", ");
+		}
+		System.out.println();
+		
+		tv.receiveVoice("오늘 날씨 어때?");
+		
+		tv.turnOff();
+	}
+	
+}
